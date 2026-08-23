1edece7f67a04003e6e994df5b9abdb1a71d83ef
Jeremy Howard <github@jhoward.fastmail.fm>
2026-08-23 16:35:43 +1000

    Prepare wheel data during sdist metadata generation

--- build_backend.py
+++ build_backend.py
@@ -1,4 +1,5 @@
 import subprocess, sys
+from pathlib import Path
 
 import maturin
 
@@ -14,9 +15,12 @@ def build_editable(wheel_directory, config_settings=None, metadata_directory=Non
     _stage_cli()
     return maturin.build_editable(wheel_directory, config_settings, metadata_directory)
 
+def prepare_metadata_for_build_wheel(metadata_directory, config_settings=None):
+    Path("target/wheel-data").mkdir(parents=True, exist_ok=True)
+    return maturin.prepare_metadata_for_build_wheel(metadata_directory, config_settings)
+
 build_sdist = maturin.build_sdist
 get_requires_for_build_wheel = maturin.get_requires_for_build_wheel
 get_requires_for_build_editable = maturin.get_requires_for_build_editable
 get_requires_for_build_sdist = maturin.get_requires_for_build_sdist
-prepare_metadata_for_build_wheel = maturin.prepare_metadata_for_build_wheel
-prepare_metadata_for_build_editable = maturin.prepare_metadata_for_build_editable
+prepare_metadata_for_build_editable = prepare_metadata_for_build_wheel
